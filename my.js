61b406740f7a4abfc681521026da7c9caeb4491e
Marissa <Marissa@UtterbergDataDev.com>
2018-10-02 17:47:59 -0400

    Update my.js

--- my.js
+++ my.js
@@ -115,3 +115,20 @@ $('#collapseOne').on('hide.bs.collapse', function () {
     'event_label': 'CloseOne'
   });
 })
+
+// When the user scrolls down 20px from the top of the document, show a button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on this button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
